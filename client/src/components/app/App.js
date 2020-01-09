2ef6ed4787a5741eee3f77496aa1ea5734797ee1
kliuchyk <kliuchyk@gmail.com>
2020-01-09 16:49:12 +0200

    add fortawesome icons

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -4,6 +4,11 @@ import "./App.css";
 import AddProjectForm from "../add-project-form/AddProjectForm";
 import ProjectList from "../project-list/ProjectList";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faEdit, faListAlt } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faTrash, faEdit, faListAlt);
+
 const App = () => {
   return (
     <div className="App">
--- client/src/components/project-item/ProjectItem.js
+++ client/src/components/project-item/ProjectItem.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { deleteProject } from "../../services/ProjectService";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./ProjectItem.css";
 
 const ProjectItem = ({ name, id }) => {
   return (
     <div className="project">
       <header className="project-header">
-        <span className="calendar-icon">icon</span>
+        <span className="calendar-icon">
+          <FontAwesomeIcon className="faicons" icon="list-alt" />
+        </span>
         <h3 className="project-name">{name}</h3>
         <span className="actions">
-          <span className="action">Edit</span>
+          <span className="action">
+            <FontAwesomeIcon className="faicons" icon="edit" />
+          </span>
           <span className="action" onClick={() => deleteProject(id)}>
-            Delete
+            <FontAwesomeIcon className="faicons" icon="trash" />
           </span>
         </span>
       </header>
